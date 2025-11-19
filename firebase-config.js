c6c41c27c3440e340e804d2dfa399682f4f16494
marzuksuleman21-ui <marzuksuleman21@gmail.com>
2025-11-19 14:22:14 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,22 @@
+// firebase-config.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-storage.js";
 
+// ⚠️ INI CONFIG YANG BENAR UNTUK PROJECT LO
 const firebaseConfig = {
   apiKey: "AIzaSyB8QWp9JnGOFPj3Qwv_98LZbT4Xla_V_Gs",
   authDomain: "sentralcoffe-afb85.firebaseapp.com",
   projectId: "sentralcoffe-afb85",
-  storageBucket: "sentralcoffe-afb85.appspot.com",   // FIX WAJIB
+  storageBucket: "sentralcoffe-afb85.appspot.com",   // WAJIB pakai .appspot.com
   messagingSenderId: "743371589232",
   appId: "1:743371589232:web:75d35a0f1e6a7f5e9fd6b5"
 };
 
+// Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
 
+// Export supaya bisa dipakai di admin, menu, checkout, orders
 export const db = getFirestore(app);
 export const storage = getStorage(app);
